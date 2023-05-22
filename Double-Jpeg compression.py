789251a1caa7ee5172d029b31b5d1c4687477713
Akash Shanker <76723803+AkashShanker321@users.noreply.github.com>
2023-05-22 22:00:30 +0530

    Double-Jpeg compression.py

--- Double-Jpeg compression.py
+++ Double-Jpeg compression.py
@@ -1,23 +1,33 @@
-from PIL import Image, ImageChops, ImageEnhance
+import cv2
+import numpy as np
 
-def calculate_ela(image_path):
-    # Load the original image
-    original_image = Image.open(image_path)
+def compare_quantization_tables(image_path):
+    # Load the image
+    image = cv2.imread(image_path)
 
-    # Perform Error Level Analysis
-    ela_image = original_image.copy()
+    # Extract quantization tables
+    quantization_tables = []
+    for i in range(2):
+        # Read the JPEG image and extract the quantization table
+        jpeg_data = cv2.imencode('.jpg', image, [cv2.IMWRITE_JPEG_QUALITY, 90])[1].tobytes()
+        quantization_table = extract_quantization_table(jpeg_data)
+        quantization_tables.append(quantization_table)
 
-    # Calculate the absolute difference between the original and ELA images
-    ela_diff = ImageChops.difference(original_image, ela_image)
+    # Compare quantization tables
+    similarity_score = calculate_similarity_score(quantization_tables[0], quantization_tables[1])
 
-    # Convert the difference image to grayscale
-    ela_diff = ela_diff.convert("L")
+    # Determine if double compression is detected
+    if similarity_score < threshold:
+        print("Double JPEG compression detected!")
+    else:
+        print("No double JPEG compression detected.")
 
-    # Normalize the ELA image for visualization
-    ela_normalized = ImageEnhance.Contrast(ela_diff).enhance(2.0)
+def extract_quantization_table(jpeg_data):
+    # Extract quantization table from JPEG data
+    # ... implementation specific to extracting quantization table from JPEG data ...
+    return quantization_table
 
-    # Save the ELA image
-    ela_normalized.save("ela.jpg")
-
-    # Return the ELA image
-    return ela_normalized
+def calculate_similarity_score(quantization_table1, quantization_table2):
+    # Calculate the similarity score between two quantization tables
+    # ... implementation specific to calculating similarity score ...
+    return similarity_score
